7f800c3e61f1d0520a3c259a70608016f2508f94
Eliottvd <eliott.vanderstraten-waillet@student.hepl.be>
2019-10-24 14:37:33 +0200

    Ajoutez des fichiers projet.

---
+++ DALTest/Todo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTest
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+
+        public Todo()
+        {
+        }
+
+        public Todo(string name, bool done)
+        {
+            Name = name;
+            Done = done;
+        }
+
+        public Todo(string name)
+        {
+            Name = name;
+            Done = false;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ DALTest/TodoCtx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using DALTest;
+
+namespace DAL
+{
+    class TodoCtx : DbContext
+    {
+        public TodoCtx() : base()
+        {
+
+        }
+
+        public DbSet<Todo> TabDbSet { get; set; }
+    }
+}
