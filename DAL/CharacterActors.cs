4d5426c93274793b5b4e60a9806281577ddb2f25
DaniLecx <daniel-garcia-lecloux@hotmail.com>
2019-10-27 23:48:45 +0100

    Changed namespace

--- DAL/CharacterActors.cs
+++ DAL/CharacterActors.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace DAl
+namespace DAL
 {
     class CharacterActors
     {
--- DAL/DALManager.cs
+++ DAL/DALManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAl
+namespace DAL
 {
     class DALManager: IDisposable
     {
