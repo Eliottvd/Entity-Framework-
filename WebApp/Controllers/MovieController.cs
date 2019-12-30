ead9f1f41a940d4e540d6cfbf37274b82b3ce70d
Eliottvd <eliott.vanderstraten-waillet@student.hepl.be>
2019-12-30 20:58:31 +0100

    Added get list film from actor

--- WebApp/Controllers/MovieController.cs
+++ WebApp/Controllers/MovieController.cs
@@ -12,6 +12,7 @@ namespace WebApp.Controllers
 {
     public class MovieController : Controller
     {
+        private int pageSize = 10;
         private Service1Client serv = new Service1Client();
         // GET: Movie
         public ActionResult Index()
@@ -21,19 +22,53 @@ namespace WebApp.Controllers
 
         public ActionResult Actors(string actName)
         {
-            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(actName, 0, 10);
+            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(actName, 0, pageSize);
             listActors tmp = new listActors(actorDTOs);
+            ViewBag.PartialName = actName; 
+            ViewBag.nbPageActors = 0;
 
             return View(tmp);
         }
 
-        public ActionResult Details(string actName)
+        public ActionResult Next(string actName, int nbPageActors)
         {
-            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(actName,0,10);
+            nbPageActors++;
+            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(actName, nbPageActors, pageSize);
+            listActors tmp = new listActors(actorDTOs);
+            ViewBag.PartialName = actName;
+            ViewBag.nbPageActors = nbPageActors;
+
+            return View(tmp);
+        }
+
+        public ActionResult Previous(string actName, int nbPageActors)
+        {
+            nbPageActors--;
+            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(actName, nbPageActors, pageSize);
+            listActors tmp = new listActors(actorDTOs);
+            ViewBag.PartialName = actName;
+            ViewBag.nbPageActors = nbPageActors;
+
+            return View(tmp);
+        }
+
+        public ActionResult Details(string fullActName)
+        {
+            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(fullActName, 0,10);
             ActorDTO act = actorDTOs.First();
             List<FilmDTO> filmDTOs = serv.GetListFilmsByIdActor(act.ActorId);
             FilmDTO tmp = filmDTOs.First();
             WebApp.Models.Movie MovModel = new Models.Movie(tmp);
+
+            return View(MovModel);
+        }
+
+        public ActionResult Movies(string fullActName)
+        {
+            List<ActorDTO> actorDTOs = serv.FindListActorByPartialActorName(fullActName, 0, 10);
+            ActorDTO act = actorDTOs.First();
+            List<FilmDTO> filmDTOs = serv.GetListFilmsByIdActor(act.ActorId);
+            WebApp.Models.listMovies MovModel = new listMovies(filmDTOs);
             return View(MovModel);
         }
     }
